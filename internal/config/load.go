4c6821ec0be6175b8706b9ef6dd6dfa4e992cdd4
Shahov Artem <shahov9ad@gmail.com>
2026-04-23 15:23:53 +0700

    update configs

--- internal/config/load.go
+++ internal/config/load.go
@@ -7,12 +7,15 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// LoadConfig - загружает конфигурацию из YAML-файла
 func LoadConfig(pathToFile string) (*Config, error) {
+	// Получаем абсолютный путь к файлу
 	filename, err := filepath.Abs(pathToFile)
 	if err != nil {
 		return nil, err
 	}
 
+	// Читаем содержимое файла
 	yamlFile, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -20,6 +23,7 @@ func LoadConfig(pathToFile string) (*Config, error) {
 
 	var cfg Config
 
+	// Разбираем YAML в структуру Config
 	err = yaml.Unmarshal(yamlFile, &cfg)
 	if err != nil {
 		return nil, err
