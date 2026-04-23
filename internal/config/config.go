f645167d5bdfab8e7664c069fa979cbcf9c84eb0
Shahov Artem <shahov9ad@gmail.com>
2026-04-23 17:50:33 +0700

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,33 +1,28 @@
 package config
 
-// Config - основная структура конфигурации приложения
 type Config struct {
-	IP   string `yaml:"ip"`   // IP-адрес сервера
-	Port int    `yaml:"port"` // Порт сервера
+	IP   string `yaml:"ip"`
+	Port int    `yaml:"port"`
 
-	API     APIConfig     `yaml:"api"`     // Конфигурация API
-	Usecase UsecaseConfig `yaml:"usecase"` // Конфигурация бизнес-логики
-	DB      DBConfig      `yaml:"db"`      // Конфигурация базы данных
+	API     api     `yaml:"api"`
+	Usecase usecase `yaml:"usecase"`
+	DB      db      `yaml:"db"`
 }
 
-// APIConfig - конфигурация для API-слоя
-type APIConfig struct {
-	SecretKey             string `yaml:"secret_key"`               // Секретный ключ для JWT
-	AccessTokenTTLMinutes int    `yaml:"access_token_ttl_minutes"` // Время жизни access-токена в минутах
-	RefreshTokenTTLDays   int    `yaml:"refresh_token_ttl_days"`   // Время жизни refresh-токена в днях
+type api struct {
+	SecretKey         string `yaml:"secret_key"`
+	FrontAddress      string `yaml:"front_address"`
+	CommentStaticPath string `yaml:"image_path"`
 }
 
-// UsecaseConfig - конфигурация для слоя бизнес-логики
-type UsecaseConfig struct {
-	DefaultMessage string `yaml:"default_message"` // Сообщение по умолчанию для health-check
+type usecase struct {
+	DefaultMessage string `yaml:"default_message"`
 }
 
-// DBConfig - конфигурация для подключения к базе данных
-type DBConfig struct {
-	Host     string `yaml:"host"`     // Хост базы данных
-	Port     int    `yaml:"port"`     // Порт базы данных
-	User     string `yaml:"user"`     // Пользователь базы данных
-	Password string `yaml:"password"` // Пароль базы данных
-	DBname   string `yaml:"dbname"`   // Имя базы данных
-	SSLMode  string `yaml:"sslmode"`  // Режим SSL для PostgreSQL
+type db struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	DBname   string `yaml:"dbname"`
 }
