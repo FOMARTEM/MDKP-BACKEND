340238d89955ebc668549b3301669b1bb14a7038
Shahov Artem <shahov9ad@gmail.com>
2026-04-28 01:19:20 +0700

    Потихоньку заполняем интерфейс для взаимодействия с логикой

--- internal/api/interface.go
+++ internal/api/interface.go
@@ -1,4 +1,25 @@
 package api
 
+import "github.com/FOMARTEM/MDKP-BACKEND/internal/entities"
+
 type Usecase interface {
+	// Пользователь
+	// Функция для авторизации пользователя
+	Authorize(user entities.User) (*bool, error)
+	// Функция для получения данных пользователя (без пароля) по email
+	SelectUserByEmail(email string) (*entities.User, error)
+	// Функция для обновления пароля пользователя
+	UpdateUserPassword(userID int, newPassword string) error
+	// Функция для получения данных пользователя (без пароля) по id
+	SelectUserByID(userID int) (*entities.User, error)
+	// Получение логов
+	GetLogs(userID int, email string, startDate, endDate string, limit, offset int) ([]entities.Log, error)
+	// Получение ролей пользователей
+	GetRoles() ([]entities.Role, error)
+	// Получение возможных статусов
+	GetStatuses() ([]entities.Status, error)
+	// Создание пользователя
+	CreateUser(user entities.User) (*entities.User, error)
+	// Удаление пользователя
+	UserActiveChange(userEmail string) error
 }
