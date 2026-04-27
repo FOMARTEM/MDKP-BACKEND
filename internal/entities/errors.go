8b295f0cf62236841660a6405acbcddb578b95ec
Shahov Artem <shahov9ad@gmail.com>
2026-04-28 01:16:50 +0700

    Добавлены ошибки

--- internal/entities/errors.go
+++ internal/entities/errors.go
@@ -5,4 +5,22 @@ import "errors"
 var (
 	// Ошибка доступа
 	ErrAccessDenied = errors.New("access denied")
+	// Ошибка авторизации, не верный пароль пользователя
+	ErrInvalidPassword = errors.New("invalid password")
+	// Ошибка авторизации, пользователь не найден
+	ErrUserNotFound = errors.New("user not found")
+	// Ошибка пароль слишком длинный
+	ErrPasswordTooLong = errors.New("password too long")
+	// Ошибка пользователь с таким email уже существует
+	ErrUserAlreadyExists = errors.New("user with this email already exists")
+	// Ошибка пользователь не активен
+	ErrUserInactive = errors.New("user is inactive")
+	// Ошибка пользователь не авторизован
+	ErrUserUnauthorized = errors.New("user is unauthorized")
+	// Ошибка токена, не верный токен
+	ErrInvalidToken = errors.New("invalid token")
+	// Ошибка пароли не совпадают
+	ErrPasswordsDoNotMatch = errors.New("passwords do not match")
+	// Ошибка не верно указаны query параметры
+	ErrInvalidQueryParams = errors.New("invalid query parameters")
 )
